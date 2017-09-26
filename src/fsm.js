34a4f9b50c043445f0d4df52a234aee8c73d931f
Sergey S <volshibenik@gmail.com>
2017-09-26 17:40:12 +0300

    all done!

--- src/fsm.js
+++ src/fsm.js
@@ -6,10 +6,14 @@ class FSM {
     constructor(config) {
 			
 			if (!config) {
-				throw new Error('No config provided') 
+				throw new Error('No config provided');
 			} else {
+				
 				this.config = config;
 				this.state = config['initial'];
+				this.memo = [];
+				this.undone = [];
+				
 			}
 			
 		}
@@ -27,10 +31,16 @@ class FSM {
      * Goes to specified state.
      * @param state
      */
+	
+	//question - should changeState work if it tries to change to current state?
     changeState(state) {
 			
 			if (state in this.config['states']) {
+				
+				this.memo.push(this.state);
 				this.state = state;
+				this.undone = []; //is it correct to do like this?
+				
 			} else {
 				throw new Error('No such state');
 			}
@@ -44,7 +54,11 @@ class FSM {
     trigger(event) {
 			
 			if (event in this.config['states'][this.state]['transitions']) {
+				
+				this.memo.push(this.state);
 				this.state = this.config['states'][this.state]['transitions'][event];
+				this.undone = []; //??
+				
 			} else {
 				throw new Error('No such event in this state');
 			}
@@ -67,11 +81,27 @@ class FSM {
      * @returns {Array}
      */
     getStates(event) {
-			for (var k in this.config['states'])
+			
+			var statesArr = [];
 			
 			if (!event) {
 				
-			} else if (event in this.config['states'][this.state]['transitions'])
+				for (var k in this.config['states']) {
+					statesArr.push(k);
+				}
+				
+			} else {
+				
+				for (var k in this.config['states']) {
+					if (event in this.config['states'][k]['transitions']) {
+						statesArr.push(k);
+					} // otherwise statesArr remains empty
+				}
+				
+			}
+			
+			return statesArr;
+			
 		}
 
     /**
@@ -79,19 +109,46 @@ class FSM {
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+			
+			if (this.memo.length != 0) {
+				
+				this.undone.push(this.state);
+				this.state = this.memo.pop();
+				return true;
+				
+			}
+			return false;
+			
+		}
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+			
+			if (this.undone.length != 0) {
+
+				this.memo.push(this.state);
+				this.state = this.undone.pop();
+				return true;
+
+			}
+			return false;
+			
+		}
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+			
+			this.memo = [];
+			this.undone = [];
+			
+		}
 }
 
 module.exports = FSM;
